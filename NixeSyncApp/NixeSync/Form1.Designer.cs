e7eaea524580ef5d93d4d25e3288081adce42a32
lrosellpardo <108391559+lrosellpardo@users.noreply.github.com>
2022-06-29 09:17:40 +0200

    Add files via upload

---
+++ NixeSyncApp/NixeSync/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace NixeSync
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.cmbPorts = new System.Windows.Forms.ComboBox();
+            this.btnRefresh = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.txtDatetime = new System.Windows.Forms.TextBox();
+            this.Timer1 = new System.Windows.Forms.Timer(this.components);
+            this.btnDisconnect = new System.Windows.Forms.Button();
+            this.btnSync = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbPorts
+            // 
+            this.cmbPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbPorts.FormattingEnabled = true;
+            this.cmbPorts.Location = new System.Drawing.Point(36, 45);
+            this.cmbPorts.Name = "cmbPorts";
+            this.cmbPorts.Size = new System.Drawing.Size(132, 21);
+            this.cmbPorts.TabIndex = 0;
+            // 
+            // btnRefresh
+            // 
+            this.btnRefresh.Location = new System.Drawing.Point(337, 45);
+            this.btnRefresh.Name = "btnRefresh";
+            this.btnRefresh.Size = new System.Drawing.Size(94, 23);
+            this.btnRefresh.TabIndex = 1;
+            this.btnRefresh.Text = "Refresh";
+            this.btnRefresh.UseVisualStyleBackColor = true;
+            this.btnRefresh.Click += new System.EventHandler(this.btnRefresh_Click);
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(174, 45);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 2;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.cmdConnect_Click);
+            // 
+            // txtDatetime
+            // 
+            this.txtDatetime.Font = new System.Drawing.Font("Segoe UI", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDatetime.Location = new System.Drawing.Point(36, 96);
+            this.txtDatetime.Name = "txtDatetime";
+            this.txtDatetime.ReadOnly = true;
+            this.txtDatetime.Size = new System.Drawing.Size(519, 71);
+            this.txtDatetime.TabIndex = 3;
+            // 
+            // Timer1
+            // 
+            this.Timer1.Enabled = true;
+            this.Timer1.Interval = 1000;
+            this.Timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // btnDisconnect
+            // 
+            this.btnDisconnect.Location = new System.Drawing.Point(256, 45);
+            this.btnDisconnect.Name = "btnDisconnect";
+            this.btnDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.btnDisconnect.TabIndex = 4;
+            this.btnDisconnect.Text = "Disconnect";
+            this.btnDisconnect.UseVisualStyleBackColor = true;
+            this.btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+            // 
+            // btnSync
+            // 
+            this.btnSync.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSync.Location = new System.Drawing.Point(36, 189);
+            this.btnSync.Name = "btnSync";
+            this.btnSync.Size = new System.Drawing.Size(117, 54);
+            this.btnSync.TabIndex = 5;
+            this.btnSync.Text = "Sync";
+            this.btnSync.UseVisualStyleBackColor = true;
+            this.btnSync.Click += new System.EventHandler(this.btnSync_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Location = new System.Drawing.Point(36, 267);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.ReadOnly = true;
+            this.txtLog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtLog.Size = new System.Drawing.Size(519, 241);
+            this.txtLog.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 538);
+            this.Controls.Add(this.txtLog);
+            this.Controls.Add(this.btnSync);
+            this.Controls.Add(this.btnDisconnect);
+            this.Controls.Add(this.txtDatetime);
+            this.Controls.Add(this.btnConnect);
+            this.Controls.Add(this.btnRefresh);
+            this.Controls.Add(this.cmbPorts);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "NixieSync";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbPorts;
+        private System.Windows.Forms.Button btnRefresh;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.TextBox txtDatetime;
+        private System.Windows.Forms.Timer Timer1;
+        private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Button btnSync;
+        private System.Windows.Forms.TextBox txtLog;
+    }
+}
+
---
+++ NixeSyncApp/NixeSync/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+using System.Text.RegularExpressions;
+
+namespace NixeSync
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort ser = new SerialPort();
+     
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+
+            cmbPorts.Items.Clear();
+
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmbPorts.Items.Add(port);
+            }
+
+            cmbPorts.SelectedIndex = 0;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                cmbPorts.Items.Add(port);
+            }
+            cmbPorts.SelectedIndex = 0;
+
+            DateTime now = DateTime.Now;
+            txtDatetime.Text = now.ToLongTimeString() + " - ";
+            txtDatetime.Text += now.ToShortDateString();
+            btnDisconnect.Enabled = false;
+            btnSync.Enabled = false;
+           
+        }
+
+        private void cmdConnect_Click(object sender, EventArgs e)
+        {
+
+            if (cmbPorts.SelectedItem == null)
+            {
+                MessageBox.Show("NO SERIAL PORT SELECTED", "ERROR");
+            }
+            else
+            {
+                ser.BaudRate = 115200;
+                ser.PortName = cmbPorts.SelectedItem.ToString();
+                ser.DataBits = 8;
+                ser.StopBits = StopBits.One;
+                ser.Parity = Parity.None;
+                ser.Handshake = Handshake.None;
+                ser.ReadTimeout = 5000;
+                try
+                {
+                    ser.Open();
+
+                    if (ser.IsOpen == true)
+                    {
+                        btnRefresh.Enabled = false;
+                        btnDisconnect.Enabled = true;
+                        btnConnect.Enabled = false;
+                        cmbPorts.Enabled = false;
+                        btnSync.Enabled = true;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.ToString(), "ERROR");
+                }
+            }
+     
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            txtDatetime.Text = now.ToLongTimeString() + " - ";
+            txtDatetime.Text += now.ToShortDateString();
+
+            if(ser.IsOpen == false)
+            {
+                btnRefresh.Enabled = true;
+                btnDisconnect.Enabled = false;
+                btnConnect.Enabled = true;
+                cmbPorts.Enabled = true;
+                btnSync.Enabled = false;
+            }
+        }
+
+        private void btnDisconnect_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                ser.Close();
+                if (ser.IsOpen == false)
+                {
+                    btnConnect.Enabled = true;
+                    btnRefresh.Enabled = true;
+                    btnDisconnect.Enabled = false;
+                    cmbPorts.Enabled = true;
+                    btnSync.Enabled = false;
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.ToString(), "ERROR");
+            }
+        }
+
+        private void btnSync_Click(object sender, EventArgs e)
+        {
+            DateTime now = DateTime.Now;
+            btnSync.Enabled = false;
+            string response ="";
+            if (ser.IsOpen)
+            {
+                ser.Write("SYNC\n");
+                txtLog.Text += "SYNC ";
+                
+                try
+                {
+                    response = ser.ReadLine();
+
+                    if (response.CompareTo("WAIT FOR SYNC") == 0)
+                    {
+                        
+                        String data = string.Format("H{0}N{1}S{2}D{3}M{4}Y{5}", now.Hour, now.Minute, now.Second, now.Day, now.Month, now.Year);
+                        ser.Write(data + "\n");
+                        txtLog.Text += data + "\r\n";
+                    }
+                  
+                }
+                catch (Exception ex)
+                {
+                    txtLog.Text += "SYNC FAILED " + ex.Message + "\r\n";
+                }
+               
+
+            try {
+                    response = ser.ReadLine();
+
+                    if (response.CompareTo("SYNC OK") == 0)
+                    {
+                        txtLog.Text += "SYNC COMPLETED\r\n";
+                    }
+                    else
+                    {
+                      txtLog.Text += "SYNC FAILED\r\n";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    txtLog.Text += "SYNC FAILED "+ ex.Message + "\r\n";
+                }
+            }
+            btnSync.Enabled = true;
+
+        }
+
+       
+    }
+}
